b9c27c145d7df8d5ccdf514587881ae6c56aad28
Calvin <Calvin.Jitnaree123@gmail.com>
2020-07-23 12:23:30 +0200

    cleaner code

--- script.js
+++ script.js
@@ -2,40 +2,29 @@ let target = document.getElementById('quote');
 
 getQuotes();
 
-
 async function getQuotes() {
 
     {
         let quotesPromise = await fetch('https://thatsthespir.it/api');
         let quotes = await quotesPromise.json();
-
         let template = document.getElementById('quoteTemplate').content;
         let quoteText = document.importNode(template, true);
 
         try {
-
             quoteText.querySelector(".text").innerHTML = quotes.quote;
             if (!quotes.photo.ok && quotes.photo !== "") {
                 quoteText.querySelector(".img").src = quotes.photo;
             }
-
             else {
                 quoteText.querySelector(".img").src = "https://lh3.googleusercontent.com/proxy/iqtiD4HhefhPnwtrIHyuuswv9_5csAkq9mrJnBockltWsVLCEGeQyO1XTsMMri1yO_bOA4VWlpPJw6jkyjYM67y_osaoh3UF5DJ_aZsunw"
             }
-
             quoteText.querySelector(".author").textContent = ` - ${quotes.author}`;
             target.appendChild(quoteText);
-
             document.querySelector(".img").onerror = function () {
                 target.querySelector(".img").src = "https://lh3.googleusercontent.com/proxy/iqtiD4HhefhPnwtrIHyuuswv9_5csAkq9mrJnBockltWsVLCEGeQyO1XTsMMri1yO_bOA4VWlpPJw6jkyjYM67y_osaoh3UF5DJ_aZsunw"
             };
-
         } catch (error) {
             console.log(error);
         }
-
-
-
     }
-
 }
