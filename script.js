13fc2f8513621f1078914273986fce8b01b5abc9
Calvin <Calvin.Jitnaree123@gmail.com>
2020-07-23 12:21:54 +0200

    handle 404 error

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let target = document.getElementById('quote');
 
 getQuotes();
-document.getElementById('change').addEventListener('click', () => {
-    location.reload()
-})
+
 
 async function getQuotes() {
 
@@ -13,19 +11,31 @@ async function getQuotes() {
 
         let template = document.getElementById('quoteTemplate').content;
         let quoteText = document.importNode(template, true);
+
         try {
+
             quoteText.querySelector(".text").innerHTML = quotes.quote;
-            if (quotes.photo !== "") {
+            if (!quotes.photo.ok && quotes.photo !== "") {
                 quoteText.querySelector(".img").src = quotes.photo;
-            } else {
+            }
+
+            else {
                 quoteText.querySelector(".img").src = "https://lh3.googleusercontent.com/proxy/iqtiD4HhefhPnwtrIHyuuswv9_5csAkq9mrJnBockltWsVLCEGeQyO1XTsMMri1yO_bOA4VWlpPJw6jkyjYM67y_osaoh3UF5DJ_aZsunw"
             }
+
             quoteText.querySelector(".author").textContent = ` - ${quotes.author}`;
             target.appendChild(quoteText);
+
+            document.querySelector(".img").onerror = function () {
+                target.querySelector(".img").src = "https://lh3.googleusercontent.com/proxy/iqtiD4HhefhPnwtrIHyuuswv9_5csAkq9mrJnBockltWsVLCEGeQyO1XTsMMri1yO_bOA4VWlpPJw6jkyjYM67y_osaoh3UF5DJ_aZsunw"
+            };
+
         } catch (error) {
             console.log(error);
         }
-    
+
+
+
     }
 
 }
